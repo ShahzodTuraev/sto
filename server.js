57e7c190c3aa607c399e9fd96896e1e60f078a06
ShahzodTuraev <turayevshahzodbek@gmail.com>
2023-04-22 13:18:23 +0900

    apis are created in .env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: OPEN_API_KEY,
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
